847d86c54ac729744268dff38414e24281f01bee
Jay0x13 <dilucrag19@gmail.com>
2024-02-26 23:00:25 +0800

    Added a spotify_api.py

---
+++ spotify_api.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+import base64
+
+# For Spotify API authentication
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+
+
+def get_token():
+    # TODO: Implement a solution for getting a temporary access token
+    ...
+
