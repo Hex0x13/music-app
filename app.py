37e9dff250a4925e7762c631f081b86b7a77e0fc
Jay0x13 <dilucrag19@gmail.com>
2024-02-29 22:58:05 +0800

    Update spotify_api.py and Use its object in app.py

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request
+from flask import render_template, redirect, url_for
 import validators
+from spotify_api import SpotifyAPI
 
 
 app = Flask(__name__)
+spotify_api = SpotifyAPI()
+
 
 # When user click "download" it will execute
 @app.route("/submit", methods=['POST'])
 def submit():
+    if request.method != 'POST':
+        return redirect(url_for("index"))
     # Get the input[type="url" name="link"] value
     link = request.form.get('link')
 
     # Check if link is a valid url, if not it will redirect to index.html
     if not validators.url(link) or link == '':
         return redirect(url_for('index'))
-    return f'Submititted link: {link}'
+    playlist_id = link.split("/")[-1]
+    return spotify_api.get_playlists(playlist_id)
 
 # When user request for index.html, this is the response
 @app.route("/")
--- spotify_api.py
+++ spotify_api.py
@@ -1,13 +1,54 @@
+import json
+from requests import post, get
 from dotenv import load_dotenv
 import os
 import base64
-
-# For Spotify API authentication
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+from datetime import datetime
 
 
-def get_token():
-    # TODO: Implement a solution for getting a temporary access token
-    ...
+class SpotifyAPI:
+    __instance = None
+
+    def __new__(cls):
+        if not cls.__instance:
+            cls.__instance = super(SpotifyAPI, cls).__new__(cls)
+            cls.__instance.__initialized = False
+        return cls.__instance
+
+    def __init__(self) -> None:
+        if self.__initialized:
+            return
+        self.__initialized = True
+
+        if not load_dotenv():
+            exit()
+        self.__client_id = os.getenv('CLIENT_ID')
+        self.__client_secret = os.getenv('CLIENT_SECRET')
+        self.__expires_at = None
+        self.__access_token = None
+        self.__init_token()
+
+    def __init_token(self) -> None:
+        auth_string = f"{self.__client_id}:{self.__client_secret}"
+        auth_bytes = auth_string.encode("utf-8")
+        auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": f"Basic {auth_base64}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        data = {"grant_type": "client_credentials"}
+        result = json.loads(post(url, headers=headers, data=data).content)
+
+        self.__access_token = result.get("access_token")
+        self.__expires_at = datetime.now().timestamp() + result.get("expires_in")
+
+    def get_playlists(self, playlist_id) -> dict:
+        if self.__expires_at and datetime.now().timestamp() >= self.__expires_at:
+            self.__init_token()
+        url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+        headers = {"Authorization": f"Bearer {self.__access_token}"}
+        result = get(url, headers=headers)
+        return json.loads(result.content)
 
